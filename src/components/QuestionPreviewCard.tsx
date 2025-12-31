7573df8218f87c97fc7450ee956b121c03d92f0d
Gihan Chamila <132575646+gihanchamila@users.noreply.github.com>
2025-12-31 13:44:17 +0530

    feat(#4): add greeny rectangle to the card according to design

--- src/components/QuestionPreviewCard.tsx
+++ src/components/QuestionPreviewCard.tsx
@@ -7,10 +7,13 @@ export default function QuestionPreviewCard({
   description,
 }: Readonly<QuestionCardPreview>) {
   return (
-    <Link href={`/question/${id}`} className="question-card">
-      <h1 className="question-card-title ">{title}</h1>
+    <div className="relative group">
+      <div className="w-3 h-10 bg-green-400 absolute -left-3 top-12 -translate-y-1/2 " />
+      <Link href={`/question/${id}`} className="question-card">
+        <h1 className="question-card-title ">{title}</h1>
 
-      <p className="question-card-description">{description}</p>
-    </Link>
+        <p className="question-card-description">{description}</p>
+      </Link>
+    </div>
   );
 }

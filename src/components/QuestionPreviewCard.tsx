cf4c6f43d02516389202d32a60f716ac36e8ebf1
Heshan Thenura Kariyawasam <75155192+heshanthenura@users.noreply.github.com>
2026-01-02 13:21:37 +0530

    Revert "feat: implement theme variables and question card component styles"

--- src/components/QuestionPreviewCard.tsx
+++ src/components/QuestionPreviewCard.tsx
@@ -7,13 +7,13 @@ export default function QuestionPreviewCard({
   description,
 }: Readonly<QuestionCardPreview>) {
   return (
-    <div className="relative group">
-      <div className="w-3 h-12 bg-primary absolute -left-3 top-14 -translate-y-1/2 " />
-      <Link href={`/question/${id}`} className="question-card">
-        <h1 className="question-card-title ">{title}</h1>
+    <Link
+      href={`/question/${id}`}
+      className="flex flex-col justify-start bg-white shadow-lg rounded-xl p-5 hover:shadow-2xl transition-shadow duration-300 max-w-[450px] md:max-w-[400px] max-h-60 overflow-hidden border border-gray-100"
+    >
+      <h1 className="text-lg font-semibold truncate h-6">{title}</h1>
 
-        <p className="question-card-description">{description}</p>
-      </Link>
-    </div>
+      <p className="text-gray-700 text-sm line-clamp-3 mt-2">{description}</p>
+    </Link>
   );
 }
--- src/components/QuestionPreviewCardContainer.tsx
+++ src/components/QuestionPreviewCardContainer.tsx
@@ -26,7 +26,7 @@ export default function QuestionPreviewCardContainer() {
 
   return (
     <div className="flex justify-center items-center w-full">
-      <div className="grid justify-center lg:grid-cols-2 md:grid-cols-3 gap-x-24 gap-y-12 py-24">
+      <div className="grid justify-center md:grid-cols-3 gap-6 px-4 py-6">
         {questions ? (
           questions.map((q) => (
             <QuestionPreviewCard

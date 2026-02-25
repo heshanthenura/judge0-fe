5d9299176f0f8f413f1664a77aa46a2b02678e8f
Heshan Thenura Kariyawasam <75155192+heshanthenura@users.noreply.github.com>
2026-02-25 11:58:09 +0530

    Development (#21)
    * feat: update login form ui to match design specs
    
    * fix: apply review feedback
    
    * fix: revrt login card stroke design
    
    * fix: updated version of navbar
    
    * fix: minor fix
    
    * refactor: remove old code for a fresh start
    
    ---------
    
    Co-authored-by: Chamal Senarathna <chamals004@gmail.com>
    Co-authored-by: Thamidu-Nadun <nadunrz101@gmail.com>

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,3 @@
-import NavBar from '@/components/NavBar';
-import QuestionPreviewCardContainer from '@/components/QuestionPreviewCardContainer';
-
 export default function Home() {
-  return (
-    <div>
-      <NavBar />
-      <QuestionPreviewCardContainer />
-    </div>
-  );
+  return <div>home</div>;
 }
